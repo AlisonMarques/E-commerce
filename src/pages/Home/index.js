a406c212297ff3e36b687e1998ad2ef91647d360
Alison Marques <alison-marques@outlook.com.br>
2020-07-07 01:04:46 -0300

    Criando a estilização da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
-function Home() {
-    return <h1></h1>;
+export default function Home() {
+    return (
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/chuteira-society-nike-phantom-vision-2-academy-df-tf/06/HZM-2955-006/HZM-2955-006_zoom1.jpg?ts=1579725014&ims=326x"
+                    alt="chuteira"
+                />
+                <strong>Chuteira muito boa</strong>
+                <span>R$ 379,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/chuteira-society-nike-phantom-vision-2-academy-df-tf/06/HZM-2955-006/HZM-2955-006_zoom1.jpg?ts=1579725014&ims=326x"
+                    alt="chuteira"
+                />
+                <strong>Chuteira muito boa</strong>
+                <span>R$ 379,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/chuteira-society-nike-phantom-vision-2-academy-df-tf/06/HZM-2955-006/HZM-2955-006_zoom1.jpg?ts=1579725014&ims=326x"
+                    alt="chuteira"
+                />
+                <strong>Chuteira muito boa</strong>
+                <span>R$ 379,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/chuteira-society-nike-phantom-vision-2-academy-df-tf/06/HZM-2955-006/HZM-2955-006_zoom1.jpg?ts=1579725014&ims=326x"
+                    alt="chuteira"
+                />
+                <strong>Chuteira muito boa</strong>
+                <span>R$ 379,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/chuteira-society-nike-phantom-vision-2-academy-df-tf/06/HZM-2955-006/HZM-2955-006_zoom1.jpg?ts=1579725014&ims=326x"
+                    alt="chuteira"
+                />
+                <strong>Chuteira muito boa</strong>
+                <span>R$ 379,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/chuteira-society-nike-phantom-vision-2-academy-df-tf/06/HZM-2955-006/HZM-2955-006_zoom1.jpg?ts=1579725014&ims=326x"
+                    alt="chuteira"
+                />
+                <strong>Chuteira muito boa</strong>
+                <span>R$ 379,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
-
-export default Home;
